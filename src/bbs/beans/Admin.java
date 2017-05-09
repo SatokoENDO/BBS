82ca908d542cc1d5d41b864791a8f56e48e98cae
SatokoENDO <endo.satoko@m.alhinc.jp>
2017-05-09 18:01:46 +0900

    filter

---
+++ src/bbs/beans/Admin.java
@@ -0,0 +1,53 @@
+package bbs.beans;
+
+import java.io.Serializable;
+
+public class Admin implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+	private String loginId;
+	private String password;
+	private String name;
+	private String isLocked;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLoginId() {
+		return loginId;
+	}
+
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIsLocked() {
+		return isLocked;
+	}
+
+	public void setIsLocked(String isLocked) {
+		this.isLocked = isLocked;
+	}
+}
